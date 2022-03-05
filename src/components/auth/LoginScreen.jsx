7108955c5680a609d22a3797baf709b3a9e581fb
EngeReyes23 <engelreyes23@gmail.com>
2022-03-05 01:26:19 -0600

    por mejorar estilo y diseño de login

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>LoginScreen</h1>
-    </div>
+    <>
+      <h3 className={"auth__title"}>Login</h3>
+
+      <form className={"auth__form"}>
+        <button className={"btn btn-google"}>
+          {" "}
+          <img
+            className="google-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="google button"
+          />{" "}
+          Log in with Google
+        </button>
+
+        <hr />
+
+        <label className={"label"} htmlFor="email">
+          Email
+        </label>
+        <input
+          id="email"
+          className={"auth__input"}
+          type="email"
+          placeholder={"Email@domain"}
+          name="email"
+          autoComplete={"off"}
+        />
+        <label className={"label"} htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          className={"auth__input"}
+          type="password"
+          placeholder={"Password"}
+          name="password"
+          autoComplete={"off"}
+        />
+        <button className={"btn"} type="submit">
+          Login
+        </button>
+
+        <Link to="/auth/register" className={""}>
+          Sing up
+        </Link>
+      </form>
+    </>
   );
 };
--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -5,17 +5,17 @@ import { RegisterScreen } from "../components/auth/RegisterScreen";
 
 export const AuthRouter = () => {
   return (
-    <div>
-      <Switch>
-        {/* LOGIN */}
-        <Route exact path={"/auth/login"} component={LoginScreen} />
-
-        {/* REGISTER */}
-        <Route exact path={"/auth/register"} component={RegisterScreen} />
-
-        {/* REDIRECT TO LOGIN */}
-        <Redirect to={"/auth/login"} />
-      </Switch>
+    <div className={"auth__main"}>
+      <div className="auth__box-container">
+        <Switch>
+          {/* LOGIN */}
+          <Route exact path={"/auth/login"} component={LoginScreen} />
+          {/* REGISTER */}
+          <Route exact path={"/auth/register"} component={RegisterScreen} />
+          {/* REDIRECT TO LOGIN */}
+          <Redirect to={"/auth/login"} />
+        </Switch>
+      </div>
     </div>
   );
 };
