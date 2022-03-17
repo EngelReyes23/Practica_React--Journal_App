7246a7447c29ecaad3a2b5483546c792e795dcba
EngeReyes23 <engelreyes23@gmail.com>
2022-03-17 17:12:18 -0600

    estilos login y register terminados por el momento

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,54 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
+  const [isValid, setIsValid] = useState(false);
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const isValidEmail = (email) => {
+    const regex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    return regex.test(email);
+  };
+
   return (
-    <>
+    <form className={"auth__form"}>
       <h3 className={"auth__title"}>Login</h3>
+      <button type={"button"} className={"btn btn-google"}>
+        {" "}
+        <img
+          className="google-icon"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+          alt="google button"
+        />{" "}
+        Log in with Google
+      </button>
 
-      <form className={"auth__form"}>
-        <button className={"btn btn-google"}>
-          {" "}
-          <img
-            className="google-icon"
-            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            alt="google button"
-          />{" "}
-          Log in with Google
-        </button>
+      <label className={"label"} htmlFor="email">
+        Email
+      </label>
+      <input
+        id="email"
+        className={"auth__input"}
+        type="email"
+        placeholder={"Email@domain"}
+        name="email"
+        value={form.email}
+        autoComplete={"off"}
+        onChange={handleChange}
+      />
 
-        <hr />
+      <label className={"label"} htmlFor="password">
+        Password
+      </label>
+      <input
+        value={form.password}
+        id="password"
+        className={"auth__input"}
+        type="password"
+        placeholder={"********"}
+        name="password"
+        autoComplete={"off"}
+        onChange={handleChange}
+      />
 
-        <label className={"label"} htmlFor="email">
-          Email
-        </label>
-        <input
-          id="email"
-          className={"auth__input"}
-          type="email"
-          placeholder={"Email@domain"}
-          name="email"
-          autoComplete={"off"}
-        />
-        <label className={"label"} htmlFor="password">
-          Password
-        </label>
-        <input
-          id="password"
-          className={"auth__input"}
-          type="password"
-          placeholder={"Password"}
-          name="password"
-          autoComplete={"off"}
-        />
-        <button className={"btn"} type="submit">
-          Login
-        </button>
+      <button
+        disabled={!isValidEmail(form.email)}
+        className={`btn btn-login ${isValid && valid}`}
+        type="submit"
+      >
+        Login
+      </button>
 
-        <Link to="/auth/register" className={""}>
-          Sing up
-        </Link>
-      </form>
-    </>
+      <p className={"QuestionRegistration"}>Don´t have an account?</p>
+
+      <Link to="/auth/register" className={"btn btn-singUP"}>
+        Sing up
+      </Link>
+    </form>
   );
 };
--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>RegisterScreen</h1>
-    </div>
+    <form className={"auth__form"}>
+      <h3 className={"auth__title"}>Register</h3>
+
+      <label className={"label"} htmlFor="email">
+        Name
+      </label>
+      <input
+        id="name"
+        className={"auth__input"}
+        type="text"
+        placeholder={"Name"}
+        name="name"
+        autoComplete={"off"}
+      />
+
+      <label className={"label"} htmlFor="email">
+        Email
+      </label>
+      <input
+        id="email"
+        className={"auth__input"}
+        type="email"
+        placeholder={"Email@domain"}
+        name="email"
+        autoComplete={"off"}
+      />
+
+      <label className={"label"} htmlFor="password">
+        Password
+      </label>
+      <input
+        id="password"
+        className={"auth__input"}
+        type="password"
+        placeholder={"********"}
+        name="password"
+        autoComplete={"off"}
+      />
+      <label className={"label"} htmlFor="password">
+        Confirm Password
+      </label>
+      <input
+        id="password2"
+        className={"auth__input"}
+        type="password"
+        placeholder={"********"}
+        name="password2"
+        autoComplete={"off"}
+      />
+
+      <button className={`btn btn-login`} type="submit">
+        Register
+      </button>
+
+      <p className={"QuestionRegistration"}>Do you already have an account ?</p>
+
+      <Link to="/auth/login" className={"btn btn-singUP"}>
+        Sing in
+      </Link>
+    </form>
   );
 };
