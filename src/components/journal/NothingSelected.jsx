b2f2a7fa85fc55db62df3f67e3e3d47956872166
EngeReyes23 <engelreyes23@gmail.com>
2022-03-17 23:13:59 -0600

    creado componente nothinSelected

---
+++ src/components/journal/NothingSelected.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NothingSelected = () => {
+  return (
+    <div className={"nothing__main-content"}>
+      <p>
+        Select a journal entry <br /> to view it here.
+      </p>
+      <span className="material-icons-outlined">star_outline</span>
+    </div>
+  );
+};
