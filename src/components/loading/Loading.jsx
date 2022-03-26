5ed932800788ec363f47501448b3d61e4fe579b0
EngeReyes23 <engelreyes23@gmail.com>
2022-03-25 19:02:32 -0600

    loading global agregado

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Loading = () => (
+  <div className="loadingContainer">
+    <div className="coffee">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -10,20 +10,30 @@ import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
 import { firebase } from "../firebase/firebaseConfig";
 import { login } from "../actions/auth";
+import { Loading } from "../components/loading/Loading";
 
 export const AppRouter = () => {
   const dispatch = useDispatch();
+
+  const [ready, setReady] = useState(false);
+  const { loading } = useSelector((state) => state.ui);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       // verifica si contiene un usuario
       user?.uid && dispatch(login(user.uid, user.displayName));
     });
+
+    setReady(true);
   }, []);
+
+  if (!ready) return <Loading />;
 
   return (
     <Router>
       <div>
+        {/* loading para mostrar en cualquier pantalla */}
+        {loading && <Loading />}
         <Switch>
           <Route path={"/auth"} component={AuthRouter} />
           <Route exact path={"/"} component={JournalScreen} />
