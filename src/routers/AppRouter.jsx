03dca3808bef02e5caf74b4f9eac64d628268da3
EngeReyes23 <engelreyes23@gmail.com>
2022-03-25 18:25:41 -0600

    manteniendo el stado de la app al recargar

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -7,8 +8,19 @@ import {
 } from "react-router-dom";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { firebase } from "../firebase/firebaseConfig";
+import { login } from "../actions/auth";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      // verifica si contiene un usuario
+      user?.uid && dispatch(login(user.uid, user.displayName));
+    });
+  }, []);
+
   return (
     <Router>
       <div>
